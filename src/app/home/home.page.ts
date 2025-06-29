e07132096eef7c1b388247e97b5f860b82a8bb54
Alfredoc <alfredo10396.c@gmail.com>
2025-06-28 20:09:21 -0600

    qr reader added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ReservationService } from '../services/reservation.service';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +9,37 @@ import { Router } from '@angular/router';
   standalone: false,
 })
 export class HomePage {
+  showScanner = false;
+  scannedResult: string | null = null;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private reservationService: ReservationService) {}
 
   logout() {
-    // Clear the token from local storage
     localStorage.removeItem('loginToken');
-    // Redirect to login page after logout
     this.router.navigate(['/auth/login']);
+  }
+
+    handleScan(result: string) {
+    this.scannedResult = result;
+    this.showScanner = false;
+    console.log('Resultado del escaneo:', result);
+
+    this.reservationService.validateQR(result).subscribe({
+      next: (response) => {
+        console.log('Validación exitosa:', response);
+      },
+      error: (err) => {
+        console.error('Error en validación:', err);
+      }
+    });
+  }
+
+  abrirScanner() {
+    this.showScanner = true;
+  }
+
+  cerrarScanner() {
+    this.showScanner = false;
   }
 
 }
