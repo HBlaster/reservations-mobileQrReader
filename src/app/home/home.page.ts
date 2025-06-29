34e2b6f86923730e09f48d738bafcadf83959095
Alfredoc <alfredo10396.c@gmail.com>
2025-06-28 23:27:48 -0600

    alert with alertController

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ReservationService } from '../services/reservation.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -12,28 +13,56 @@ export class HomePage {
   showScanner = false;
   scannedResult: string | null = null;
 
-  constructor(private router: Router, private reservationService: ReservationService) {}
+  constructor(
+    private router: Router,
+    private reservationService: ReservationService,
+    private alertController: AlertController
+  ) {}
 
   logout() {
     localStorage.removeItem('loginToken');
     this.router.navigate(['/auth/login']);
   }
 
-    handleScan(result: string) {
+  handleScan(result: string) {
     this.scannedResult = result;
     this.showScanner = false;
     console.log('Resultado del escaneo:', result);
 
     this.reservationService.validateQR(result).subscribe({
-      next: (response) => {
-        console.log('Validación exitosa:', response);
+      next: (response: any) => {
+        if (response.status === 'success') {
+          this.alertController
+            .create({
+              header: 'Validación Exitosa',
+              message: 'El código QR es válido.',
+              buttons: ['OK'],
+            })
+            .then((alert) => alert.present());
+        } else if (response.status === 'checked_in') {
+          this.alertController
+            .create({
+              header: 'Error',
+              message: 'El código QR ya ha sido utilizado.',
+              buttons: ['OK'],
+            })
+            .then((alert) => alert.present());
+        }
+        console.log('response:', response);
       },
       error: (err) => {
         console.error('Error en validación:', err);
-      }
+        this.alertController
+          .create({
+            header: 'Error',
+            message:
+              'Ocurrió un error al validar el código QR. Por favor, inténtelo de nuevo.',
+            buttons: ['OK'],
+          })
+          .then((alert) => alert.present());
+      },
     });
   }
-
   abrirScanner() {
     this.showScanner = true;
   }
@@ -41,5 +70,4 @@ export class HomePage {
   cerrarScanner() {
     this.showScanner = false;
   }
-
 }
